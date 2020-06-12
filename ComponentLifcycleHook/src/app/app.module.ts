1545acd01b22f43b787a0b85bb053e008eb6a748
Mayura-146 <mayura.sonar@outlook.com>
2020-06-12 22:07:05 +0530

    Lifecycle commit

--- ComponentLifcycleHook/src/app/app.module.ts
+++ ComponentLifcycleHook/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentHookComponent } from './parent-hook/parent-hook.component';
+import { ChildHookComponent } from './child-hook/child-hook.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentHookComponent,
+    ChildHookComponent
 
 
   ],
---
+++ ComponentLifcycleHook/src/app/parent-hook/parent-hook.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-hook',
+  templateUrl: './parent-hook.component.html',
+  styleUrls: ['./parent-hook.component.css']
+})
+export class ParentHookComponent implements OnInit {
+  employee = {
+    id: 23,
+    name: 'Mayura',
+    location: 'Bangalore'
+  };
+ShowChild=true;
+counter = 0;
+increment() {
+    this.counter++;
+  }
+
+decrement() {
+    this.counter--;
+  }
+constructor() { }
+
+ngOnInit() {
+  }
+
+  UpdateDetails() {
+    this.employee = {
+      id: 45,
+      name: 'Sumit',
+      location: 'Pune'
+    };
+  }
+
+  Hidshow(){
+    this.ShowChild=!this.ShowChild;
+  }
+}
