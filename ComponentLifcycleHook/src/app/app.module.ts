7f42343cf385bc5a909cec77631d1b21c1997f1a
Mayura-146 <mayura.sonar@outlook.com>
2020-06-12 10:31:54 +0530

    c

--- ComponentLifcycleHook/src/app/app.module.ts
+++ ComponentLifcycleHook/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Demo1Component } from './demo1/demo1.component';
-import { Demo2Component } from './demo2/demo2.component';
-import { Demo3Component } from './demo3/demo3.component';
+im;
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Demo1Component,
-    Demo2Component,
-    Demo3Component
+    AppComponent
+
 
   ],
   imports: [
---
+++ ComponentLifcycleHook/src/app/chilc-child/chilc-child.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chilc-child',
+  templateUrl: './chilc-child.component.html',
+  styleUrls: ['./chilc-child.component.css']
+})
+export class ChilcChildComponent implements OnInit {
+@Input () employee;
+  constructor() { }
+check()
+private
+  ngOnInit() {
+  }
+
+}
