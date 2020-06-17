ea613d20aa1761abfd5d1359be28336a10695dd2
Mayura-146 <mayura.sonar@outlook.com>
2020-06-18 00:44:12 +0530

    fds

--- AngularForms/src/app/reactive-form/reactive-form.component.ts
+++ AngularForms/src/app/reactive-form/reactive-form.component.ts
@@ -4,20 +4,26 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
   signupForm: FormGroup;
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      name: new FormControl('Mayura', [Validators.required, Validators.minLength(3)]),
+      name: new FormControl('Mayura', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
       username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')]),
-      confirmPassword: new FormControl('', [Validators.required])
-          });
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
   }
 
   onSubmit() {
