79ae3e9d66f9de14422f854a08ab4adb2c6e77c7
Mayura-146 <mayura.sonar@outlook.com>
2020-06-18 01:18:35 +0530

    chnages

--- AngularForms/src/app/reactive-form/reactive-form.component.ts
+++ AngularForms/src/app/reactive-form/reactive-form.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { CustomValidationService } from "../custom-validation.service";
 
 @Component({
-  selector: 'app-reactive-form',
-  templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css'],
+  selector: "app-reactive-form",
+  templateUrl: "./reactive-form.component.html",
+  styleUrls: ["./reactive-form.component.css"],
 })
 export class ReactiveFormComponent implements OnInit {
   signupForm: FormGroup;
-  constructor() {}
+  constructor(private service: CustomValidationService) {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      name: new FormControl('Mayura', [
+      name: new FormControl("Mayura", [
         Validators.required,
         Validators.minLength(3),
       ]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [
+      email: new FormControl("", [Validators.required, Validators.email]),
+      username: new FormControl(
+        "",
+        [Validators.required],
+        this.service.UserNameValidation.bind(this.service)
+      ),
+      password: new FormControl("", [
         Validators.required,
-        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'),
+        Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"),
       ]),
-      confirmPassword: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl("", [Validators.required]),
     });
   }
 
