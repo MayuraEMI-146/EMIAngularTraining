97613ea027b4c51d7f50f0fb6ec18c012b3bfd41
Mayura-146 <mayura.sonar@outlook.com>
2020-07-02 12:31:26 +0530

    Removed old path ofcomponents

--- AngularRecaps/src/app/app.module.ts
+++ AngularRecaps/src/app/app.module.ts
@@ -1,34 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './Features/home/home.component';
-import { AboutUsComponent } from './Features/about-us/about-us.component';
-import { MenuComponent } from './Features/menu/menu.component';
-import { AddEmployeeComponent } from './Features/Employees/add-employee/add-employee.component';
-import { EditEmployeeComponent } from './Features/Employees/edit-employee/edit-employee.component';
-import { ViewEmployeeComponent } from './Features/Employees/view-employee/view-employee.component';
-import { EditEmployeesComponent } from './Features/Employees/EmployeeModule/edit-employees/edit-employees.component';
-import { LeavesComponent } from './Features/Employees/leaves/leaves.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./Features/home/home.component";
+import { AboutUsComponent } from "./Features/about-us/about-us.component";
+import { MenuComponent } from "./Features/menu/menu.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutUsComponent,
-    MenuComponent,
-    AddEmployeeComponent,
-    EditEmployeeComponent,
-    ViewEmployeeComponent,
-    EditEmployeesComponent,
-    LeavesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutUsComponent, MenuComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
