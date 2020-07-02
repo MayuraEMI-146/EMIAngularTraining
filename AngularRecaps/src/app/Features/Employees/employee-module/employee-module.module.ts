0faa419bc8d9b241a2ed4836d5e19eee6ae99439
Mayura-146 <mayura.sonar@outlook.com>
2020-07-02 12:35:09 +0530

    added router outlet to employeedetails

--- AngularRecaps/src/app/Features/Employees/employee-module/employee-module.module.ts
+++ AngularRecaps/src/app/Features/Employees/employee-module/employee-module.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { EmployeeModuleRoutingModule } from './employee-module-routing.module';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-import { ViewEmployeeComponent } from './view-employee/view-employee.component';
-import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
-
+import { EmployeeModuleRoutingModule } from "./employee-module-routing.module";
+import { AddEmployeeComponent } from "./add-employee/add-employee.component";
+import { EditEmployeeComponent } from "./edit-employee/edit-employee.component";
+import { ViewEmployeeComponent } from "./view-employee/view-employee.component";
+import { EmployeeDetailsComponent } from "./employee-details/employee-details.component";
 
 @NgModule({
-  declarations: [AddEmployeeComponent, EditEmployeeComponent, ViewEmployeeComponent, EmployeeDetailsComponent],
-  imports: [
-    CommonModule,
-    EmployeeModuleRoutingModule
-  ]
+  declarations: [
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    ViewEmployeeComponent,
+    EmployeeDetailsComponent,
+  ],
+  imports: [CommonModule, EmployeeModuleRoutingModule],
 })
-export class EmployeeModuleModule { }
+export class EmployeeModuleModule {
+  constructor() {
+    console.log("Employee Module Loaded!!");
+  }
+}
